80df2fd9cb41dc9b80b3ccb37cd51ea6bb4029b7
FatDev <fathil.arham@gmail.com>
2020-11-18 08:38:35 +0700

    Id card null

--- resources/views/registration/person-update.blade.php
+++ resources/views/registration/person-update.blade.php
@@ -58,7 +58,11 @@
                 </div>
 
                 <div class="col-md-6 col-sm-12 mt-3">
+                @if ($person->card_path != null)
                     <img src="{{ asset('storage/id_cards/' . $person->card_path) }}" class="img-fluid" alt="">
+                @else
+                    <h4>Foto KTP belum ada</h4>
+                @endif
                 </div>
 
                 @include('components.form-section-end')
